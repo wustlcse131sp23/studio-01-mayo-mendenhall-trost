9ca56b81c2834df454d2448a12a1f2348487259a
gyvnnmendenhall <gyvnnmendenhall@gyvnns-mbp.dhcp.wustl.edu>
2023-01-26 12:50:15 -0600

    finished studio

--- src/studio1/HiFour.java
+++ src/studio1/HiFour.java
@@ -17,6 +17,8 @@ public class HiFour {
 		String s2 = in.nextLine();
 		System.out.print("Enter value for arg 3: ");
 		String s3 = in.nextLine();
+		System.out.print("Greetings " + s0 + ", " + s1 + ", " + s2 + ", and " + s3);
+
 		//
 		// Say hello to the names in s0 through s3.
 		//
